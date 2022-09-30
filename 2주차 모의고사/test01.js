c4c86c7e177dbb09c2dfa6399c604e04fb9dbc95
kmdet1235 <73745204+kmdet1235@users.noreply.github.com>
2022-09-30 10:16:07 +0900

    Delete 2주차 모의고사 directory

--- 2주차 모의고사/test01.js
+++
@@ -1,9 +0,0 @@
-function solution(arr1) {
-  let answer = 45;
-  for (let i = 0; i < arr1.length; i++) {
-    answer -= arr1[i];
-  }
-  return answer;
-}
-let arr1 = [1, 3, 5, 9, 2, 4, 8, 0];
-console.log(solution(arr1));
