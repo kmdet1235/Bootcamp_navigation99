dd1ed9c30b59bc8f75b5e9b992c2fcba38619594
kmdet1235 <73745204+kmdet1235@users.noreply.github.com>
2022-10-03 23:33:34 +0900

    Delete spa_mall directory

--- spa_mall/modules/math.js
+++
@@ -1,7 +0,0 @@
-// modules/math.js
-function add(a, b) {
-  return a + b;
-}
-
-// modules/math.js
-module.exports = add;
--- spa_mall/modules/run.js
+++
@@ -1,4 +0,0 @@
-// modules/run.js
-const add = require("./math"); // './' 이것은 상대경로를 의미함. 즉 modules라는 폴더를 의미함
-console.log(add(3, 4));
-// Print: 7
